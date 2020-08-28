7b3f4c6f359470c553480b86187449f1de3c4fc0
pgallowaaaaay <70017227+pgallowaaaaay@users.noreply.github.com>
2020-08-28 08:07:23 -0400

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,101 +1,123 @@
+# Hitting With Projectiles
+
+def on_player1_button_a_pressed():
+    global laser
+    laser = sprites.create_projectile_from_sprite(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . 8 6 6 9 . . . . . . . 
+                    . . . . . 8 6 6 9 . . . . . . . 
+                    . . . . . 8 6 6 9 . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        playership,
+        75,
+        0)
+controller.player1.on_button_event(ControllerButton.A,
+    ControllerButtonEvent.PRESSED,
+    on_player1_button_a_pressed)
+
+def on_on_overlap(sprite, otherSprite):
+    sprite.destroy()
+    otherSprite.destroy(effects.fire, 100)
+    # As-destr-oid
+    info.change_score_by(1)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_on_overlap)
+
+# Accurate Life Counter
+
+def on_on_overlap2(sprite, otherSprite):
+    otherSprite.destroy()
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+asteroid2: Sprite = None
+laser: Sprite = None
+myEnemy = 0
+playership: Sprite = None
 # Prerequisites and Setting Up
 info.set_score(0)
 info.set_life(3)
-
 # Player's Sprite
 playership = sprites.create(img("""
-
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . b b . . . . . . . . . . . .
-    . b b b b . . . . . . . . . . .
-    . b b b b b . . . . . . . . . .
-    . . b b b b b . . . . . . . . .
-    . . b b b b b b . . . . . . . .
-    . . b b b b b b b . . . . . . .
-    . . b b b b b b . . . . . . . .
-    . . b b b b b . . . . . . . . .
-    . b b b b b . . . . . . . . . .
-    . b b b b . . . . . . . . . . .
-    . . b b . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-"""))
-playership.set_position (20, 120/2)
-playership.set_flag(SpriteFlag.StayInScreen, True)
-#Getting Controls
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . b b . . . . . . . . . . . . 
+            . b b b b . . . . . . . . . . . 
+            . b b b b b . . . . . . . . . . 
+            . . b b b b b . . . . . . . . . 
+            . . b b b b b b . . . . . . . . 
+            . . b b b b b b b . . . . . . . 
+            . . b b b b b b . . . . . . . . 
+            . . b b b b b . . . . . . . . . 
+            . b b b b b . . . . . . . . . . 
+            . b b b b . . . . . . . . . . . 
+            . . b b . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    0)
+playership.set_position(20, 120 / 2)
+playership.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
+# Getting Controls
 controller.move_sprite(playership, 140, 140)
-
-#Enemies
+playership.set_kind(SpriteKind.player)
+# Enemies
 asteroid = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . c c . . . . .
-    . . . . . . . . c c . c c c . .
-    . . . . . . . c c b c b c c c .
-    . . . . . . c c c c c c b c c .
-    . . . c c c c c b c c b . c . .
-    . . c . b b c c c c c c c c . .
-    . . c b c c c c c c . b c . . .
-    . . c c c c c . b c c c . . . .
-    . . . c c c . c . . . . . . . .
-    . . . . c c c . . . . . . . . .
-    . . . . . c . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-"""))
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . c c . . . . . 
+            . . . . . . . . c c . c c c . . 
+            . . . . . . . c c b c b c c c . 
+            . . . . . . c c c c c c b c c . 
+            . . . c c c c c b c c b . c . . 
+            . . c . b b c c c c c c c c . . 
+            . . c b c c c c c c . b c . . . 
+            . . c c c c c . b c c c . . . . 
+            . . . c c c . c . . . . . . . . 
+            . . . . c c c . . . . . . . . . 
+            . . . . . c . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    myEnemy)
 asteroid.set_velocity(-55, 0)
 asteroid.set_position(scene.screen_width(), randint(0, scene.screen_height()))
+asteroid.set_kind(SpriteKind.enemy)
 
 def on_update_interval():
-    asteroid = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . c c . . . . .
-    . . . . . . . . c c . c c c . .
-    . . . . . . c c c b c b c c c .
-    . . . . . c . c c c c c b c c .
-    . . . c c . c c b c c b . c . .
-    . . c . b b c c c c c c c c . .
-    . . c b c c c c c c . b c . . .
-    . . c c c c c . b c c c . . . .
-    . . . c c c . c . . . . . . . .
-    . . . . c c c . . . . . . . . .
-    . . . . . c . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    """))
-    asteroid.set_velocity(-55, 0)
-    asteroid.set_position(scene.screen_width(), randint(0, scene.screen_height()))
-
-
-
-
-
+    global asteroid2
+    asteroid2 = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . c c . . . . . 
+                    . . . . . . . . c c . c c c . . 
+                    . . . . . . c c c b c b c c c . 
+                    . . . . . c . c c c c c b c c . 
+                    . . . c c . c c b c c b . c . . 
+                    . . c . b b c c c c c c c c . . 
+                    . . c b c c c c c c . b c . . . 
+                    . . c c c c c . b c c c . . . . 
+                    . . . c c c . c . . . . . . . . 
+                    . . . . c c c . . . . . . . . . 
+                    . . . . . c . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        0)
+    asteroid2.set_velocity(-55, 0)
+    asteroid2.set_position(scene.screen_width(), randint(0, scene.screen_height()))
 game.on_update_interval(500, on_update_interval)
-
-#Hitting With Projectiles 
-
-def on_button_event_a_pressed():
-    laser = sprites.create_projectile_from_sprite(img("""
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . 8 6 6 9 . . . . . . .
-        . . . . . 8 6 6 9 . . . . . . .
-        . . . . . 8 6 6 9 . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-    """), playership, 75, 0)
-controller.player1.on_button_event(ControllerButton.A, ControllerButtonEvent.PRESSED, on_button_event_a_pressed)
--- main.ts
+++ main.ts
@@ -1,91 +1,109 @@
+//  Hitting With Projectiles
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function on_player1_button_a_pressed() {
+    
+    laser = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . 8 6 6 9 . . . . . . . 
+                    . . . . . 8 6 6 9 . . . . . . . 
+                    . . . . . 8 6 6 9 . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        `, playership, 75, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.fire, 100)
+    //  As-destr-oid
+    info.changeScoreBy(1)
+})
+//  Accurate Life Counter
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap2(sprite: Sprite, otherSprite: Sprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+let asteroid2 : Sprite = null
+let laser : Sprite = null
+let myEnemy = 0
+let playership : Sprite = null
 //  Prerequisites and Setting Up
 info.setScore(0)
 info.setLife(3)
 //  Player's Sprite
-let playership = sprites.create(img`
-
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . b b . . . . . . . . . . . .
-    . b b b b . . . . . . . . . . .
-    . b b b b b . . . . . . . . . .
-    . . b b b b b . . . . . . . . .
-    . . b b b b b b . . . . . . . .
-    . . b b b b b b b . . . . . . .
-    . . b b b b b b . . . . . . . .
-    . . b b b b b . . . . . . . . .
-    . b b b b b . . . . . . . . . .
-    . b b b b . . . . . . . . . . .
-    . . b b . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`)
+playership = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . b b . . . . . . . . . . . . 
+            . b b b b . . . . . . . . . . . 
+            . b b b b b . . . . . . . . . . 
+            . . b b b b b . . . . . . . . . 
+            . . b b b b b b . . . . . . . . 
+            . . b b b b b b b . . . . . . . 
+            . . b b b b b b . . . . . . . . 
+            . . b b b b b . . . . . . . . . 
+            . b b b b b . . . . . . . . . . 
+            . b b b b . . . . . . . . . . . 
+            . . b b . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    `, 0)
 playership.setPosition(20, 120 / 2)
 playership.setFlag(SpriteFlag.StayInScreen, true)
-// Getting Controls
+//  Getting Controls
 controller.moveSprite(playership, 140, 140)
-// Enemies
+playership.setKind(SpriteKind.Player)
+//  Enemies
 let asteroid = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . c c . . . . .
-    . . . . . . . . c c . c c c . .
-    . . . . . . . c c b c b c c c .
-    . . . . . . c c c c c c b c c .
-    . . . c c c c c b c c b . c . .
-    . . c . b b c c c c c c c c . .
-    . . c b c c c c c c . b c . . .
-    . . c c c c c . b c c c . . . .
-    . . . c c c . c . . . . . . . .
-    . . . . c c c . . . . . . . . .
-    . . . . . c . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`)
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . c c . . . . . 
+            . . . . . . . . c c . c c c . . 
+            . . . . . . . c c b c b c c c . 
+            . . . . . . c c c c c c b c c . 
+            . . . c c c c c b c c b . c . . 
+            . . c . b b c c c c c c c c . . 
+            . . c b c c c c c c . b c . . . 
+            . . c c c c c . b c c c . . . . 
+            . . . c c c . c . . . . . . . . 
+            . . . . c c c . . . . . . . . . 
+            . . . . . c . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    `, myEnemy)
 asteroid.setVelocity(-55, 0)
 asteroid.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
+asteroid.setKind(SpriteKind.Enemy)
 game.onUpdateInterval(500, function on_update_interval() {
-    let asteroid = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . c c . . . . .
-    . . . . . . . . c c . c c c . .
-    . . . . . . c c c b c b c c c .
-    . . . . . c . c c c c c b c c .
-    . . . c c . c c b c c b . c . .
-    . . c . b b c c c c c c c c . .
-    . . c b c c c c c c . b c . . .
-    . . c c c c c . b c c c . . . .
-    . . . c c c . c . . . . . . . .
-    . . . . c c c . . . . . . . . .
-    . . . . . c . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    `)
-    asteroid.setVelocity(-55, 0)
-    asteroid.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
-})
-// Hitting With Projectiles 
-controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function on_button_event_a_pressed() {
-    let laser = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . 8 6 6 9 . . . . . . .
-        . . . . . 8 6 6 9 . . . . . . .
-        . . . . . 8 6 6 9 . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-    `, playership, 75, 0)
+    
+    asteroid2 = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . c c . . . . . 
+                    . . . . . . . . c c . c c c . . 
+                    . . . . . . c c c b c b c c c . 
+                    . . . . . c . c c c c c b c c . 
+                    . . . c c . c c b c c b . c . . 
+                    . . c . b b c c c c c c c c . . 
+                    . . c b c c c c c c . b c . . . 
+                    . . c c c c c . b c c c . . . . 
+                    . . . c c c . c . . . . . . . . 
+                    . . . . c c c . . . . . . . . . 
+                    . . . . . c . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        `, 0)
+    asteroid2.setVelocity(-55, 0)
+    asteroid2.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
 })
